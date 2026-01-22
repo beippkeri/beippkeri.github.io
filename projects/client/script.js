ecf51c73fb2a6aedd54f64344dc783addedf3186
beippkeri <bentettris@hotmail.com>
2026-01-22 16:44:53 -0500

    finished the gallery and added buttons

--- projects/client/script.js
+++ projects/client/script.js
@@ -2,8 +2,6 @@
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-const img = document.getElementById('carosel1');
-const img2 = document.getElementById('carosel2');
 const galleryList = [
     'media/before1.png',
     'media/after1.png',
@@ -12,10 +10,15 @@ const galleryList = [
 ]
 let currentGalleryIndex = 0;
 function carosel() {
+    const img = document.getElementById('carosel1');
+const img2 = document.getElementById('carosel2');
+    console.log('Changing Carousel Images');
 if (img != null && img2 != null) {
     img.src = galleryList[currentGalleryIndex];
     img2.src = galleryList[(currentGalleryIndex + 1) % galleryList.length];
     currentGalleryIndex = (currentGalleryIndex + 2) % galleryList.length;
+    console.log('Carousel images updated');
+    return true;
 } else {
     console.log('Carousel image elements not found');
     return false;
