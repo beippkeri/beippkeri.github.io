b380f3e9ea7fde0d74cd94aea68bc06dc34ccb63
beippkeri <bentettris@hotmail.com>
2026-01-22 12:39:58 -0500

    making the actual gallery

--- projects/client/script.js
+++ projects/client/script.js
@@ -2,14 +2,29 @@
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+const img = document.getElementById('carosel1');
+const img2 = document.getElementById('carosel2');
+const galleryList = [
+    'media/before1.png',
+    'media/after1.png',
+    'media/before2.png',
+    'media/after2.png'
+]
+let currentGalleryIndex = 0;
+function carosel() {
+if (img != null && img2 != null) {
+    img.src = assetList[0];
+    img2.src = assetList[1];
+} else {
+    console.log('Carousel image elements not found');
+}
+}
 const assetList = [
     'media/red.png',
     'media/purple.png',
     'media/green.png'
 ]
 let currentAssetIndex = 0;
-
-console.log('Page loaded');
 
 async function unfade(element, duration) {
     let opacity = 0;
