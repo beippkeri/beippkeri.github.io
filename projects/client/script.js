835fe73bb85d2e10e95eab278c4e8723bd32ca96
beippkeri <bentettris@hotmail.com>
2026-01-22 21:01:24 -0500

    started about page and cleaned up index page

--- projects/client/script.js
+++ projects/client/script.js
@@ -1,7 +1,4 @@
 
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 const galleryList = [
     'media/before1.png',
     'media/after1.png',
@@ -24,34 +21,3 @@ if (img != null && img2 != null) {
     return false;
 }
 }
-const assetList = [
-    'media/red.png',
-    'media/purple.png',
-    'media/green.png'
-]
-let currentAssetIndex = 0;
-
-async function unfade(element, duration) {
-    let opacity = 0;
-    const interval = 50;
-    const increment = interval / duration;
-    while (opacity < 1) {
-        opacity += increment;
-        if (opacity > 1) opacity = 1;
-        element.style.opacity = opacity;
-        await delay(interval);
-    }
-}
-async function main() {
-    console.log('Changing Image');
-    const img = document.getElementById('switch');
-    if (img != null) {
-        currentAssetIndex = (currentAssetIndex + 1) % assetList.length;
-        img.src = assetList[currentAssetIndex];
-        unfade(img, 2000);
-    }else{
-        console.log('Image element not found');
-        return false;
-    }
-}
-setInterval(main, 4000);
