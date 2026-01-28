e1248c8dfce92c3afa48eadf592b4e93cd0978df
beippkeri <bentettris@hotmail.com>
2026-01-28 03:11:58 -0500

    remembered my inner feelings and process on my code

--- projects/client/process.md
+++ projects/client/process.md
@@ -7,7 +7,7 @@ Getting the contact page to work was past my abilities, as I don't know how to u
 Finished everything else
 All of the communication was over text, everything was shown.
 
-Modification 1 Dialogue (week 1)
+Modification 1 Dialogue (week 1) Sat, Jan 17
 ME - Do you like the website?
 CL - It's good??
 ME - What should I change? 
@@ -15,7 +15,7 @@ CL - You should improve semantics.
 Modification 1 - Improve Semantics
 semantics improved
 
-Modification 2 Dialogue (week 2)
+Modification 2 Dialogue (week 2) Sat, Jan 24
 ME - Do you think the gallery page looks a little dry
 CL - Yeah
 ME - Should I spice it up a bit
@@ -23,7 +23,7 @@ CL - Yes
 Modification 2 - Spice up the gallery
 spiced up the gallery
 
-Modification 3 (week 3)
+Modification 3 (week 3) Tue, Jan 27
 ME - Do you like the side pages, or do they look to similar to the original ones?
 CL - Yeah, but they do look very similar, you should place links there
 ME - Ok
--- projects/client/script.js
+++ projects/client/script.js
@@ -4,20 +4,20 @@ const galleryList = [
     'media/after1.png',
     'media/before2.png',
     'media/after2.png'
-]
+] // add more images if thy please
 let currentGalleryIndex = 0;
 function carosel() {
     const img = document.getElementById('carosel1');
-const img2 = document.getElementById('carosel2');
+const img2 = document.getElementById('carosel2'); // both images at a time
     console.log('Changing Carousel Images');
 if (img != null && img2 != null) {
     img.src = galleryList[currentGalleryIndex];
     img2.src = galleryList[(currentGalleryIndex + 1) % galleryList.length];
-    currentGalleryIndex = (currentGalleryIndex + 2) % galleryList.length;
+    currentGalleryIndex = (currentGalleryIndex + 2) % galleryList.length; // move forward by 2 images
     console.log('Carousel images updated');
     return true;
 } else {
-    console.log('Carousel image elements not found');
+    console.log('Carousel image elements not found'); // hopefully will never happen
     return false;
 }
 }
