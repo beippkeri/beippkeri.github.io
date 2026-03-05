e9bdce96db642185a33ba8b8a529ed03aae2cc83
beippkeri <bentettris@hotmail.com>
2026-03-05 13:08:12 -0500

    initialized variables

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,11 +1,11 @@
 let gameActive = true; //this variable is required. 
                        //to stop the game, set it to false.
 
-//Declare your other global variables here
+//config
 let warpUnlocked = false;
 let currentLocation = null;
 
-//If you need, add any "helper" functions here
+//Commands and other helper functions
 function helpCommand() {
     print("Available commands:"
         + "\n\tType an option to trigger a location change"
@@ -20,7 +20,7 @@ function helpCommand() {
     );
 }
 
-//Make one function for each location
+//Main Areas
 function cloudGate() {
     currentLocation = "cloudGate";
     clear();
@@ -52,8 +52,11 @@ function cloudTown() {
     clear();
     print("\nYou have entered the Cloud Town!");
     print("\nWhere do you want to go next?"
-         + "\n\tCloudGate"
-         + "\n\tCloudCastle (locked)"
+        + "\n\tCloudGate"
+        + "\n\tCloudTownEast"
+        + "\n\tCloudTownWest"
+        + "\n\tCloudTownSouth"
+        + "\n\tCloudTownSquare"
     );
     
     function processInput(input){
@@ -73,7 +76,138 @@ function cloudTown() {
     }
     waitForInput(processInput);
 }
-
+// Cloud Town Areas
+function cloudTownWest() {
+    currentLocation = "cloudTownWest";
+    clear();
+    print("\nYou have moved to the west side of Cloud Town!");
+    print("\nWhere do you want to go next?"
+        + "\n\tCloudTown"
+        + "\n\tStore"
+    );
+    
+    function processInput(input){
+        if (input.toLowerCase() === "cloudtown") {
+            cloudTown();
+        } else if (input[0] === ".") {
+            // Handle help command
+            if (input.toLowerCase() === ".help") {
+                helpCommand();
+            } else {
+                stayHere();
+            }
+        } else {
+            stayHere();
+            waitThenCall(cloudTownWest);
+        }
+    }
+    waitForInput(processInput);
+}
+function cloudTownEast() {
+    currentLocation = "cloudTownEast";
+    clear();
+    print("\nYou have moved to the East side of Cloud Town!");
+    print("\nWhere do you want to go next?"
+        + "\n\tCloudTown"
+    );
+    
+    function processInput(input){
+        if (input.toLowerCase() === "cloudgate") {
+            cloudGate();
+        } else if (input[0] === ".") {
+            // Handle help command
+            if (input.toLowerCase() === ".help") {
+                helpCommand();
+            } else {
+                stayHere();
+            }
+        } else {
+            stayHere();
+            waitThenCall(cloudTown);
+        }
+    }
+    waitForInput(processInput);
+}
+function cloudTownWest() {
+    currentLocation = "cloudTownEast";
+    clear();
+    print("\nYou have moved to the east side of Cloud Town!");
+    print("\nWhere do you want to go next?"
+        + "\n\tCloudTown"
+        + "\n\tLibrary"
+    );
+    
+    function processInput(input){
+        if (input.toLowerCase() === "cloudtown") {
+            cloudTown();
+        } else if (input[0] === ".") {
+            // Handle help command
+            if (input.toLowerCase() === ".help") {
+                helpCommand();
+            } else {
+                stayHere();
+            }
+        } else {
+            stayHere();
+            waitThenCall(cloudTownEast);
+        }
+    }
+    waitForInput(processInput);
+}
+function cloudTownSouth() {
+    currentLocation = "cloudTownSouth";
+    clear();
+    print("\nYou have moved to the south side of Cloud Town!");
+    print("\nWhere do you want to go next?"
+        + "\n\tCloudTown"
+        + "\n\tGym"
+    );
+    
+    function processInput(input){
+        if (input.toLowerCase() === "cloudtown") {
+            cloudTown();
+        } else if (input[0] === ".") {
+            // Handle help command
+            if (input.toLowerCase() === ".help") {
+                helpCommand();
+            } else {
+                stayHere();
+            }
+        } else {
+            stayHere();
+            waitThenCall(cloudTownSouth);
+        }
+    }
+    waitForInput(processInput);
+}
+function cloudTownSquare() {
+    currentLocation = "cloudTownSquare";
+    clear();
+    print("\nYou have moved to the center of Cloud Town!");
+    print("\nWhere do you want to go next?"
+        + "\n\tCloudTown"
+        + "\n\tCraftingShop"
+        + "\n\tWarp Zone"
+        + "\n\tBank"
+    );
+    
+    function processInput(input){
+        if (input.toLowerCase() === "cloudtown") {
+            cloudTown();
+        } else if (input[0] === ".") {
+            // Handle help command
+            if (input.toLowerCase() === ".help") {
+                helpCommand();
+            } else {
+                stayHere();
+            }
+        } else {
+            stayHere();
+            waitThenCall(cloudTownWest);
+        }
+    }
+    waitForInput(processInput);
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
