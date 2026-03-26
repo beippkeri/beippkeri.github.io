5fb36765568094917c678b20bdee5632ea276eb7
beippkeri <bentettris@hotmail.com>
2026-03-26 12:26:08 -0400

    loops

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -1,19 +1,41 @@
 let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
-
-//fill in code for canvas1 here
+ctx.fillStyle = 'black'
+for (let blockY = 0; blockY <= 200; blockY += 100) {
+    for (let blockX = 0; blockX < 800; blockX += 100) {
+        ctx.fillRect(blockX, blockY, 50, 20);
+    }
+}
 
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");
+ctx.fillStyle = 'black'
 
-//fill in code for canvas2 here
+for (let blockY = 0; blockY < 800; blockY += 100) {
+    for (let blockX = 0; blockX < blockY; blockX += 100) {
+        ctx.fillRect(blockX, blockY, 50, 20);
+    }
+}
 
 canvas = document.getElementById("canvas3");
 ctx = canvas.getContext("2d");
+ctx.fillStyle = 'black'
 
-//fill in code for canvas3 here
+for (let blockY = 0; blockY < 800; blockY += 100) {
+    for (let blockX = 0; blockX < 800; blockX += 100) {
+        if (!(blockX == blockY)){
+        ctx.fillRect(blockX, blockY, 50, 20);
+        }
+    }
+}
 
 canvas = document.getElementById("canvas4");
 ctx = canvas.getContext("2d");
+ctx.fillStyle = 'black'
 
-//fill in code for canvas4 here
+for (let blockY = 0; blockY <= 800; blockY += 200) {
+    for (let blockX = 0; blockX <= 800; blockX += 200) {
+        ctx.fillRect(blockX, blockY, 100, 100);
+        ctx.fillRect(blockX+100, blockY+100, 100, 100);
+    }
+}
